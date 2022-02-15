353a44ec09ebc540ec084e2d22feaf35a857d9be
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-15 23:06:01 +0530

    Added:Calculating salary till condition matches

--- src/com/emloyeewage/oops/EmployeeWage.java
+++ src/com/emloyeewage/oops/EmployeeWage.java
@@ -2,48 +2,61 @@ package com.emloyeewage.oops;
 
 import java.util.Random;
 
-public class EmployeeWage {
-	public static void main(String[] args) {
-		System.out.println("Welcome to empwage");
-
-		int empCheck = Employee.empCheck();
-		Employee.empSalary(empCheck);
-	}
-
-}
 class Employee {
-	static final int is_full_time = 1;
-	static final int is_part_time = 2;
-	static final int Emp_Rate_Per_Hr = 20;
-	static final int Emp_Rate_For_Month = 20;
+	static final int is_full_time = 1;	
+	static final int is_Part_time = 2;	
+	static final int Emp_Rate_Per_Day = 20;	
+	static final int Emp_Rate_For_Month = 20;	
+	static final int Total_Working_Hrs_Month = 100;	
+	private static final int IS_FULL_TIME = 0;
 
 	public static int empCheck() {
 		Random random = new Random();
 		int empCheck = random.nextInt(3);
 		return empCheck;
 	}
-	public static int empSalary(int empCheck) {
+
+	public static int empWrkHrs(int empCheck) {
 		int empWrkHrs;
 		switch (empCheck) {
 		case is_full_time -> empWrkHrs = 8;
-		case is_part_time -> empWrkHrs = 4;
+		case is_Part_time -> empWrkHrs = 4;
 		default -> empWrkHrs = 0;
 		}
-		int empWage = empWrkHrs * Emp_Rate_Per_Hr;
-		System.out.println("empWage = " + empWage);
+		return empWrkHrs;
+	}
+
+	public static int empWage(int empWrkHrs) {
+		int empWage = empWrkHrs * Emp_Rate_Per_Day;
 		return empWage;
 	}
+
 	public static int totalEmpSalary() {
 		int day = 0;
 		int totalEmpSalary = 0;
+		int empWrkHrs = 0;
 		int empWage = 0;
 		int empCheck = 0;
-		while (day < Emp_Rate_For_Month) {
-		    empCheck = empCheck();
-		    empWage = empSalary(empCheck);
-		    ++day;
-		    totalEmpSalary += empWage;
+		int totalEmpWrkHrs = 0;
+		while (day < Emp_Rate_For_Month && totalEmpWrkHrs < Total_Working_Hrs_Month) {
+			empCheck = empCheck();
+			empWrkHrs = empWrkHrs(empCheck);
+			empWage = empWage(empWrkHrs);
+			totalEmpWrkHrs += empWrkHrs;
+			++day;
+			totalEmpSalary += empWage;
 		}
 		return totalEmpSalary;
-	    }
+	}
+
+}
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		int totalEmpSalary;
+		System.out.println("Welcome to empwage");
+		totalEmpSalary = Employee.totalEmpSalary();
+		System.out.println("totalEmpSalary = " + totalEmpSalary);
+	}
+
 }

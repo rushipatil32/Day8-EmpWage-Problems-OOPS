9ffebaa5c44e5780c954625703736590041ef48b
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-15 22:22:08 +0530

    Added:Calculating the salary for a month

--- src/com/emloyeewage/oops/EmployeeWage.java
+++ src/com/emloyeewage/oops/EmployeeWage.java
@@ -15,13 +15,14 @@ class Employee {
 	static final int is_full_time = 1;
 	static final int is_part_time = 2;
 	static final int Emp_Rate_Per_Hr = 20;
+	static final int Emp_Rate_For_Month = 20;
 
 	public static int empCheck() {
 		Random random = new Random();
 		int empCheck = random.nextInt(3);
 		return empCheck;
 	}
-	public static void empSalary(int empCheck) {
+	public static int empSalary(int empCheck) {
 		int empWrkHrs;
 		switch (empCheck) {
 		case is_full_time -> empWrkHrs = 8;
@@ -30,5 +31,19 @@ class Employee {
 		}
 		int empWage = empWrkHrs * Emp_Rate_Per_Hr;
 		System.out.println("empWage = " + empWage);
+		return empWage;
 	}
+	public static int totalEmpSalary() {
+		int day = 0;
+		int totalEmpSalary = 0;
+		int empWage = 0;
+		int empCheck = 0;
+		while (day < Emp_Rate_For_Month) {
+		    empCheck = empCheck();
+		    empWage = empSalary(empCheck);
+		    ++day;
+		    totalEmpSalary += empWage;
+		}
+		return totalEmpSalary;
+	    }
 }

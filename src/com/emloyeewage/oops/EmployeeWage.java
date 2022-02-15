f2f561f890fac241083a9fab5555db6f521051c5
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-15 21:49:14 +0530

    Added:calculating the salary of full time as well as part time employee

--- src/com/emloyeewage/oops/EmployeeWage.java
+++ src/com/emloyeewage/oops/EmployeeWage.java
@@ -13,11 +13,12 @@ public class EmployeeWage {
 }
 class Employee {
 	static final int is_full_time = 1;
-	static final int Emp_Rate_Per_Hr= 20;
+	static final int is_part_time = 2;
+	static final int Emp_Rate_Per_Hr = 20;
 
 	public static int empCheck() {
 		Random random = new Random();
-		int empCheck = random.nextInt(2);
+		int empCheck = random.nextInt(3);
 		return empCheck;
 	}
 
@@ -25,6 +26,8 @@ class Employee {
 		int empWrkHrs = 0;
 		if (empCheck == is_full_time) {
 			empWrkHrs = 8;
+		} else if (empCheck == is_part_time) {
+			empWrkHrs = 4;
 		} else {
 			empWrkHrs = 0;
 		}
